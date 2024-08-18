f0d4b7d290d2dbd37db8d2b0d565ba36ce0d0c99
astr0n0mer <42691857+astr0n0mer@users.noreply.github.com>
2024-08-18 17:26:14 +0530

    fix: redirects 404s

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from contextlib import asynccontextmanager
 from datetime import datetime
+from http import HTTPStatus
 
 from afk_parser.afk_parser import AFKParser
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, responses
 from starlette import status
 
 from lib.models import AFKRecord, SlackPostRequestBody
@@ -52,7 +53,12 @@ def get_response(records: list[AFKRecord]) -> dict:
 
 @app.get("/health-check")
 def read_health():
-    return {"server_started_at": server_started_at.strftime("%Y-%m-%d %H:%M:%S"), "status": "active"}
+    return {"server_started_at": server_started_at.strftime("%Y-%m-%d %H:%M:%S"), "status": "ok"}
+
+
+@app.exception_handler(HTTPStatus.NOT_FOUND)
+async def not_found_handler(request: Request, exc: Exception):
+    return responses.RedirectResponse(url="/health-check")
 
 
 @app.post("/v1/slack_bot")
